41a24dc0deda51e1a18431288837d82e8916269c
Mikolaj Przybysz <mikolaj.przybysz@home24.de>
2015-11-11 13:41:41 +0100

    Added cache interface

---
+++ src/Cache/SimpleCacheInterface.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * This file is part of InterfaceLib.
+ *
+ * (c) "Mikolaj Przybysz" <mikolaj.przybysz@home24.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace InterfaceLib\Cache;
+
+/**
+ * It is expected that the implementation can differ
+ * in method arguments and return types.
+ *
+ * @since Interface available since Release 1.0.0
+ */
+interface SimpleCacheInterface
+{
+    /**
+     * Get value from cache
+     *
+     * @param string $key key assigned to stored value
+     *
+     * @return string
+     */
+    public function get($key);
+
+    /**
+     * Cache payload under given identifier
+     *
+     * @param string $key   identifier
+     * @param string $value payload
+     *
+     * @return void
+     */
+    public function set($key, $value);
+
+    /**
+     * Delete cache entry under given identifier
+     *
+     * @param string $key identifier
+     *
+     * @return void
+     */
+    public function del($key);
+}
--- src/Command/SimpleCommandInterface.php
+++ src/Command/SimpleCommandInterface.php
@@ -7,11 +7,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace InterfaceLib\Lock;
+namespace InterfaceLib\Command;
 
 /**
- * Log implementing this interface is expected
- * to provide simple string logging method.
+ * Command implementing this interface is expected
+ * to provide output based on input arguments.
  * It is expected that the implementation can differ
  * in method arguments and return types.
  *
